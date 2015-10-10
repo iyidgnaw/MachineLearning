00b6b7a7b678d228c40d7516d5c0c2cbf82c99c8
lizkgithub <lizhaokang2008@126.com>
2015-10-10 15:26:51 +0800

    test

--- RNN_lizk/test.py
+++ RNN_lizk/test.py
@@ -4,90 +4,137 @@ Minimal character-level Vanilla RNN model. Written by Andrej Karpathy (@karpathy
 BSD License
 """
 import numpy as np
-import xlrd
-import xlwt
-data = xlrd.open_workbook('C:/Users/lizk/Desktop/2/movie/test.xlsx') # should be simple plain text file
-table = data.sheets()[0]
-data = table.col_values(1)
-chars = list(set(data))
-data_size, movie_size = len(data), len(chars)
-print 'data has %d characters, %d unique.' % (data_size, movie_size)
-char_to_ix = { ch:i for i,ch in enumerate(chars) }
-ix_to_char = { i:ch for i,ch in enumerate(chars) }
+import pickle
+import random
+
+
+#read
+f1 = open("./data_NextBasket.txt", "rb")
+data = pickle.load(f1)
+f1.close()
+# data.get(3)
+f1 = open("./data_idList.txt", "rb")
+listfre = pickle.load(f1)
+f1.close()
+f1 = open("./data_custmTest1010.txt", "rb")
+listcust = pickle.load(f1)
+f1.close()
+print len(listcust)
 
 # hyperparameters
-hidden_size = 100 # size of hidden layer of neurons
-seq_length = 25 # number of steps to unroll the RNN for
+hidden_size = 20 # size of hidden layer of neurons
 learning_rate = 1e-1
+goods_size = 1559
+itert = 0
 
 # model parameters
-Wxh = np.random.randn(hidden_size, movie_size)*0.01 # input to hidden
-Whh = np.random.randn(hidden_size, hidden_size)*0.01 # hidden to hidden
-Why = np.random.randn(movie_size, hidden_size)*0.01 # hidden to output
-bh = np.zeros((hidden_size, 1)) # hidden bias
-by = np.zeros((movie_size, 1)) # output bias
+u = np.random.randn(hidden_size, hidden_size)*0.01 # input to hidden
+w = np.random.randn(hidden_size, hidden_size)*0.01 # hidden to hidden
+t = np.random.randn(goods_size, hidden_size)*0.01 # one-hot to embedding
 
-def lossFun(inputs, targets, hprev):
-  """
-  inputs,targets are both list of integers.
-  hprev is Hx1 array of initial hidden state
-  returns the loss, gradients on model parameters, and last hidden state
-  """
-  x, y, p = [], [], []
-  hl=hprev
-  loss = 0
+
+def sigmoid(x):                  #sigmoid function
+    return 1.0/(1+np.exp(-x))
+
+
+def lossFun(inputs, targets, negtargets, hprev)                    :#loss function    everybasket
+    loss = 0
+    mid = 0
+    midn = 0
+    midt = 0
+    hl = np.copy(hprev)
+    x = np.zeros((goods_size,1)) # encode in 1-of-k representation
+
+
   # forward pass
-
-  x = np.zeros((movie_size,1)) # encode in 1-of-k representation
-  x[inputs][0] = 1
-
-  h = np.tanh(np.dot(Wxh, x) + np.dot(Whh, hl) + bh) # hidden state
-  y = np.dot(Why, h) + by # unnormalized log probabilities for next chars
-  p = np.exp(y) / np.sum(np.exp(y)) # probabilities for next chars
-
-  loss=-np.log(p[targets][0])
-  print loss
-
-  dWxh, dWhh, dWhy = np.zeros_like(Wxh), np.zeros_like(Whh), np.zeros_like(Why)
-  dbh, dby = np.zeros_like(bh), np.zeros_like(by)
-
-
-  dy = np.copy(p)
-  dy[targets][0] -= 1 # backprop into y
-  for i in range(len(dy)):
-      dy[i][0]=-dy[i][0]
-  dWhy += learning_rate*np.dot(dy, h.T)
-  dby += dy
-  dh = np.dot(Why.T, dy) # backprop into h
-  dhraw = (1-h * h) * dh # backprop through tanh nonlinearity
-  dbh += dhraw
-  dWxh += learning_rate*np.dot(dhraw, x.T)
-  dWhh += learning_rate*np.dot(dhraw, hl.T)
-  for dparam in [dWxh, dWhh, dWhy, dbh, dby]:
-    np.clip(dparam, -5, 5, out=dparam) # clip to mitigate exploding gradients
-  return loss, dWxh, dWhh, dWhy, dbh, dby, h
+    for i in inputs:
+        x[i-1][0] = 1
+    h = sigmoid(np.dot(np.dot(u,t.T),x) + np.dot(w,hl)) # hidden state
+    for i in targets:                 #calculate the loss
+        xt = np.zeros((goods_size,1))
+        xt[i-1][0] = 1
+        loss += np.log(sigmoid(np.dot(np.dot(xt.T,t),h)))
+    for i in negtargets:
+        xn = np.zeros((goods_size,1))
+        xn[i-1][0] = 1
+        loss += np.log(1 - sigmoid(np.dot(np.dot(xn.T,t),h)))
+    print loss
 
 
-mWxh, mWhh, mWhy = np.zeros_like(Wxh), np.zeros_like(Whh), np.zeros_like(Why)
-mbh, mby = np.zeros_like(bh), np.zeros_like(by) # memory variables for Adagrad
+    du, dw, dt = np.zeros_like(u), np.zeros_like(w), np.zeros_like(t)
 
 
-for i in range(len(data)-1):
-  if i == 0:
-    hprev = np.ones((hidden_size,1)) # reset RNN memory
-  inputs = char_to_ix[data[i]]
-  targets = char_to_ix[data[i+1]]
-  loss, dWxh, dWhh, dWhy, dbh, dby, hprev = lossFun(inputs, targets, hprev)
-  # Wxh, Whh, Why, bh, by=dWxh, dWhh, dWhy, dbh, dby
 
-  # # perform parameter update with Adagrad
-  # for param, dparam, mem in zip([Wxh, Whh, Why, bh, by],
-  #                               [dWxh, dWhh, dWhy, dbh, dby],
-  #                               [mWxh, mWhh, mWhy, mbh, mby]):
-  #    mem += dparam * dparam
-  #    param += -learning_rate * dparam / np.sqrt(mem + 1e-8) # adagrad update
+    for i in targets:                   #loss to hide
+        xt = np.zeros((goods_size,1))
+        xt[i-1][0] = 1
+        # mid += 1-sigmoid(np.dot((np.dot(np.dot(x.T, t), h)), np.dot(x.T,t))).T
+        mid += (1-sigmoid(np.dot(np.dot(xt.T,t),h)))*np.dot(xt.T,t).T
+        midt += (1-sigmoid(np.dot(np.dot(xt.T, t), h))) * np.dot(xt,h.T)
 
-  
-  for param, dparam in zip([Wxh, Whh, Why, bh, by],
-                                [dWxh, dWhh, dWhy, dbh, dby]):
-    param += -learning_rate * dparam # adagrad update
+        # print np.shape(mid)  #1, 1559
+    for i in negtargets:
+        xn= np.zeros((goods_size,1))
+        xn[i-1][0] = 1
+        mid -= sigmoid(np.dot(np.dot(xn.T, t), h))*np.dot(xn.T,t).T
+        midn += sigmoid(np.dot(np.dot(xn.T, t), h)) * np.dot(xn,h.T)
+    dw = np.dot(mid*h*(1-h),hl.T)
+    du += np.dot(mid*h*(1-h), np.dot(t.T,x).T)         #x how to choose   x x+1
+    dt += np.dot(np.dot(u.T,mid*h*(1-h)),x.T).T
+
+    dt +=midt-midn
+    # dt = np.dot(np.dot((1 - sigmoid(np.dot(np.dot(x.T,t), h))),x),h.T) + midn + \
+    #      np.dot(np.dot(mid.T*hl*(1-hl), u.T), x.T)
+    hl = h
+    return loss, du, dw, dt, hl
+
+
+def negasamp(targets):
+    list2 = listfre
+    for i in targets:
+        list2 = filter(lambda a: a != i, list2)
+    # print targets
+    # print list2
+    negtargets = []
+    for i in range(50):
+        negtargets.append(random.choice(list2))
+    return negtargets
+
+
+while True:
+    itert += 1
+    for i in range(len(listcust)-1):
+        customer = data[listcust[i]]
+        hprev = np.zeros((hidden_size, 1))
+
+        print "customer"
+        print i
+        print customer
+
+        for j in range(len(customer)-1):
+
+            inputs = customer[j]
+            targets = customer[j+1]
+            negtargets = negasamp(targets)
+            loss, du, dw, dt, hprev = lossFun(inputs, targets, negtargets, hprev)
+    # for j in range(len(inputs)-1):
+    #     # print "basket"
+    #     # print j
+    #     loss, du, dw, dt, hprev = lossFun(inputs, targets, negtargets, hprev)
+            for param, dparam in zip([u, w, t],
+                                     [du, dw, dt]):
+                param += learning_rate * dparam # adagrad update
+        if i%5==0:
+            print "iter %d"%itert
+            hint="This is round %d"%i
+            print hint
+            print u
+            print w
+            print t
+
+
+
+    
+
+
+
