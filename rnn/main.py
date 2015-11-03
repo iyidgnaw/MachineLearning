440befe8b626ca6c8045b394cebfa5302f2049fa
iyidgnaw <imwangdiyi@gmail.com>
2015-11-03 10:02:51 +0800

    10.16

--- rnn/main.py
+++ rnn/main.py
@@ -16,15 +16,23 @@ f1.close()
 f1 = open("./data_idList.txt", "rb")
 listfre = pickle.load(f1)
 f1.close()
-f1 = open("./data_custmTrain.txt", "rb")
+f1 = open("./list_cust4List.txt", "rb")
 listcust = pickle.load(f1)
+f1.close()
+f1 = open("./dic_custbasketnum.txt", "rb")
+custbasketnum = pickle.load(f1)
+f1.close()
+f1 = open("./list_product_id.txt", "rb")
+product_id = pickle.load(f1)
 f1.close()
 print len(listcust)
 
 # hyperparameters
-hidden_size = 20 # size of hidden layer of neurons
+hidden_size = 10 # size of hidden layer of neurons
 learning_rate = 1e-1
 goods_size = 1559
+itert = 0
+top = 50
 
 # model parameters
 u = np.random.randn(hidden_size, hidden_size)*0.01 # input to hidden
@@ -57,7 +65,6 @@ def lossFun(inputs, targets, negtargets, hprev)                    :#loss functi
         xn = np.zeros((goods_size,1))
         xn[i-1][0] = 1
         loss += np.log(1 - sigmoid(np.dot(np.dot(xn.T,t),h)))
-    print loss
 
 
     du, dw, dt = np.zeros_like(u), np.zeros_like(w), np.zeros_like(t)
@@ -99,31 +106,94 @@ def negasamp(targets):
         negtargets.append(random.choice(list2))
     return negtargets
 
-for i in range(len(listcust)-1):
-    customer = data[listcust[i]]
-    hprev = np.zeros((hidden_size, 1))
 
-    print "customer"
-    print i
-    print customer
-
-    for j in range(len(customer)-1):
-
+def predict(customer, u, w, t):
+    right = 0
+    hl = np.zeros((hidden_size, 1))
+    x = np.zeros((goods_size,1)) # encode in 1-of-k representation
+    xt = np.zeros((goods_size,1)) # encode in 1-of-k representation
+    # rank = np.zeros((20,2))
+    rank = [[0]*2 for row in range(top)]
+    for j in range(int(len(customer)*0.7)+1):
         inputs = customer[j]
-        targets = customer[j+1]
-        negtargets = negasamp(targets)
-        loss, du, dw, dt, hprev = lossFun(inputs, targets, negtargets, hprev)
+        for i in inputs:
+            x[i-1][0] = 1
+        h = sigmoid(np.dot(np.dot(u,t.T),x) + np.dot(w,hl)) # hidden state
+    for j in range((int(len(customer)*0.7)+1), len(customer)):
+        targets = customer[j]
+        for i in product_id:
+            xt[i-1][0] = 1
+            valuet = sigmoid(np.dot(np.dot(xt.T,t),h))
+            xt = np.zeros((goods_size,1)) 
+            rank = sort(rank)
+            if valuet > rank[0][1]:
+                rank[0][0] = i
+                rank[0][1] = valuet
+        for i in targets:
+            for j in range(top):
+
+                if i == rank[j][0]:
+                    right += 1
+                    print "!"
+        hl = h
+        x = np.zeros((goods_size,1)) 
+        for i in targets:
+            x[i-1][0] = 1
+        h = sigmoid(np.dot(np.dot(u,t.T),x) + np.dot(w,hl)) # hidden state
+    return right
+
+
+def sort(a):
+    for k in range(len(a)):
+        (a[k][0], a[k][1]) = (a[k][1], a[k][0])
+    a.sort()
+    for k in range(len(a)):
+        (a[k][0], a[k][1]) = (a[k][1], a[k][0])
+    return a
+
+
+
+while True:
+    right = 0
+    rightpre= -100
+    itert += 1
+    for i in range(len(listcust)-1):
+        customer = data[listcust[i]]
+        hprev = np.zeros((hidden_size, 1))
+
+        print "customer"
+        print i
+        for j in range(int(len(customer)*0.7)+1):
+            inputs = customer[j]
+            targets = customer[j+1]
+
+            negtargets = negasamp(targets)
+            loss, du, dw, dt, hprev = lossFun(inputs, targets, negtargets, hprev)
     # for j in range(len(inputs)-1):
     #     # print "basket"
     #     # print j
     #     loss, du, dw, dt, hprev = lossFun(inputs, targets, negtargets, hprev)
-        for param, dparam in zip([u, w, t],
-                             [du, dw, dt]):
-            param += learning_rate * dparam # adagrad update
-    if i%50==0:
-       hint="This is round %d"%i
-       print hint
-       print u
-       print w
-       print t
-        
+            for param, dparam in zip([u, w, t],[du, dw, dt]):
+                param += learning_rate * dparam # adagrad update
+    print "iter %d"%itert 
+    try:
+        for i in range(len(listcust)-1):
+            customer = data[listcust[i]]
+            right += predict(customer, u, w, t)
+            print "This is customer %d"%i
+            print right
+        strright=str(right)+" "
+        result=open("result.txt", "a")
+        result.write(strright)
+        pickle.dump(u,open("resultu.txt", "w"))
+        pickle.dump(w,open("resultw.txt", "w"))
+        pickle.dump(t,open("resultt.txt", "w"))
+    except:
+        continue
+
+
+
+    
+
+
+
