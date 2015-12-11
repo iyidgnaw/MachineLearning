4831f38dd4ba6f8d61f0a766fdc1e3a7e12d0af6
iyidgnaw <imwangdiyi@gmail.com>
2015-12-11 12:42:31 +0800

    change the dataset

--- BPR/preprocess.py
+++ BPR/preprocess.py
@@ -1,11 +1,23 @@
-data=open('./u.data')
+data=open('./up3.txt','r')
+order=open('./order.txt','a')
 lines=data.readlines()
+down3=open('./down3.txt','r')
+lines1=down3.readlines()
 train=open('train.txt','a')
 test=open('test.txt','a')
 count=0
+dic={}
+for i in range(943):
+	dic[i+1]=[]
 for line in lines:
-	count+=1
-	if count<=50000:
-		train.write(line)
-	else:
-		test.write(line)
+	record = line.split("	")
+	dic[int(record[0])].append(line)
+for i in range(943):
+	for j in range(len(dic[i+1])):
+		if j<len(dic[i+1])/2:
+			test.write(dic[i+1][j])
+		else:
+			train.write(dic[i+1][j])
+for i in lines1:
+	train.write(i)
+
