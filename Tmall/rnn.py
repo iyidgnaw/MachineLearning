039bbb1f7656d2954b02b8d78b0dd536210a6e52
lizkgithub <lizhaokang2008@126.com>
2015-12-17 23:38:33 +0800

    1217

--- Tmall/rnn.py
+++ Tmall/rnn.py
@@ -4,7 +4,6 @@ import random
 import math
 import xlrd
 
-<<<<<<< HEAD
 train_data = xlrd.open_workbook('train.xlsx')
 test_data = xlrd.open_workbook('test.xlsx')
 train_table = train_data.sheets()[0]
@@ -26,6 +25,15 @@ def sigmoid(x):
 	output = 1/(1+np.exp(-x))
 	return output
 
+#return a list including len(user_cart) negative items
+def negative(user_cart):
+	negtargets = []
+	list2 = product_id
+	negtargets=random.sample(list2, 80)
+	for m in user_cart:
+		negtargets = filter(lambda a: a != m, negtargets)
+	negtargets = negtargets[0:len(user_cart)]
+	return negtargets
 
 def train(user_cart,u ,v ,w):
 	hl = np.copy(hprev)
@@ -59,15 +67,59 @@ def train(user_cart,u ,v ,w):
 		hl = h
 	return u,v,w,x,h,loss
 
-#return a list including len(user_cart) negative items
-def negative(user_cart):
-	negtargets = []
-	list2 = product_id
-	negtargets=random.sample(list2, 80)
-	for m in user_cart:
-		negtargets = filter(lambda a: a != m, negtargets)
-	negtargets = negtargets[0:len(user_cart)]
-	return negtargets
+
+def predict(all_cart):
+	reat1 = 0
+	reat2 = 0
+	reat5 = 0
+	reat10 = 0
+	relevant = 0
+	for user_cart in all_cart:
+		i = 0
+		hl = np.copy(hprev)
+		for item_id in user_cart:
+			item = x[item_id]
+			hid_input = np.dot(item, u)+ np.dot(hl, w)
+			h = sigmoid(hid_input)
+			i += 1
+			hl = h
+			if i>int(len(user_cart)*0.8):
+				break
+		for j in range(i, len(user_cart)):
+			relevant += 1
+			predict_matrix = np.dot(h, v)
+			rank_index = np.argsort(predict_matrix, axis=1) #ordered by row small->big return index
+			rank_index = rank_index[:, -10:np.shape(rank_index)[1]]
+			if rank_index[0][-1] == user_cart[j+1]:
+				reat1 += 1
+				reat2 += 1
+				reat5 += 1
+				reat10 += 1
+				continue
+			if rank_index[0][-2] == user_cart[j+1]:
+				reat2 += 1
+				reat5 += 1
+				reat10 += 1
+				continue
+			for i in rank_index[:,-5:-2]:
+				reat5 += 1
+				reat10 += 1
+				continue
+			if i in rank_index[:,-10:-5]:
+				reat10 += 1
+				continue
+	recall_1 = reat1/relevant
+	recall_2 = reat2/relevant
+	recall_5 = reat5/relevant
+	recall_10 = reat10/relevant
+
+	print recall_1, recall_2, recall_5, recall_10
+
+
+
+
+
+
 
 
 for iter in range(1000):
@@ -78,60 +130,10 @@ for iter in range(1000):
 		user_cart = all_cart[i]
 		user_cart = user_cart[0:int(0.8*len(user_cart))]
 		u,v,w,x,h,loss=train(user_cart,u ,v ,w)
-=======
-data = xlrd.open_workbook('data.xlsx')
-
-data = data.sheets()[0]
-user_data = list(set(data.col_values(0)))
-item_data = list(set(data.col_values(1)))
-user_size, product_size = len(user_data), len(item_data)
-previous=1.0
-all_cart=[]
-user_cart=[]
-print type(data.col_values(0)[0])
-print user_size,product_size
-print type(previous)
-for i in range(len(data.col_values(1))):
-    if data.col_values(0)[i]!=previous:
-        all_cart.append(user_cart)
-        previous=data.col_values(0)[i]
-        user_cart=[]
-    else:
-        user_cart.append(data.col_values(1)[i])
-print all_cart[0]
 
 
+	predict(all_cart)
 
-
-# learning_rate = 0.1
-# lamda=0.01
-# hidden_size = 10
-# u=np.random.randn(hidden_size, hidden_size)*0.5
-# v=np.random.randn(hidden_size, product_size)*0.5
-# w=np.random.randn(hidden_size, hidden_size)*0.5
-# x=np.random.randn(hidden_size, hidden_size)*0.5
-#
-#
-# def train(user_cart,u,v,w):
-#
-#
-#
-#
-#
-#     return du,dv,dw,dus,h
-#
-# for iter in range(1000):
-#     print "Iter %d"%iter
-#     print "Training..."
-#     hiddensave=[]
-#     for i in range(len(all_cart)):
-#         user_cart=all_cart[i]
-#         du,dv,dw,dus,h=train(user_cart)
-#         for param, dparam in zip([u, w, v,user_cart],[du, dw, dv,dus]):
-# 	 			param += -learning_rate * dparam
-#         all_cart[i]=user_cart
-#         hiddensave.append(h)
->>>>>>> f7aeecb6654f488d3c6462486c7c93cc3f6431fc
 
 
 
