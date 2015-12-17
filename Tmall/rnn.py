f7aeecb6654f488d3c6462486c7c93cc3f6431fc
iyidgnaw <wangdiyi@Myself.local>
2015-12-17 17:06:37 +0800

    12.17 17:06

--- Tmall/rnn.py
+++ Tmall/rnn.py
@@ -4,13 +4,26 @@ import random
 import math
 import xlrd
 
-train_data = xlrd.open_workbook('train.xlsx')
-test_data = xlrd.open_workbook('test.xlsx')
-train_table = train_data.sheets()[0]
-test_table = train_data.sheets()[0]
-origin_user_id = list(set(table.col_values(0)))
-origin_product_id = list(set(table.col_values(1)))
-user_size, product_size = len(origin_user_id), len(origin_product_id)
+data = xlrd.open_workbook('data.xlsx')
+
+data = data.sheets()[0]
+user_data = list(set(data.col_values(0)))
+item_data = list(set(data.col_values(1)))
+user_size, product_size = len(user_data), len(item_data)
+previous=1.0
+all_cart=[]
+user_cart=[]
+print type(data.col_values(0)[0])
+print user_size,product_size
+print type(previous)
+for i in range(len(data.col_values(1))):
+    if data.col_values(0)[i]!=previous:
+        all_cart.append(user_cart)
+        previous=data.col_values(0)[i]
+        user_cart=[]
+    else:
+        user_cart.append(data.col_values(1)[i])
+print all_cart[0]
 
 
 
