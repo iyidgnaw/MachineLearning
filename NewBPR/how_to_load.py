71c8cc1c4fe674d8389d902eed8ab6870f1f6c07
iyidgnaw <wangdiyi@Myself.local>
2016-01-11 10:47:37 +0800

    data

--- NewBPR/how_to_load.py
+++ NewBPR/how_to_load.py
@@ -4,10 +4,12 @@ f2 = open("10_data.txt",'r')
 f3 = open("data.txt",'a')
 dic = pickle.load(f1)
 lines = f2.readlines()
+count=0
 for line in lines:
 	record = line.split('	')
 	longid=record[2]
-	print dic[longid]
-	string = str(record[0].split('_')[1].split(' ')[0])+"	"+str(dic[longid])
+	count+=1
+	string = str(record[0].split('_')[1].split(' ')[0])+"	"+str(dic[longid])+"\n"
 	f3.write(string)
+print count
 f3.close()
