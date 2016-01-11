c4a55874a57465c63c83427958600f4ac07f8f51
iyidgnaw <imwangdiyi@gmail.com>
2016-01-11 10:32:20 +0800

    pre

--- NewBPR/how_to_load.py
+++ NewBPR/how_to_load.py
@@ -1,4 +1,13 @@
 import pickle
-f1 = open("xxxxxxxxxxxxx.txt", "r")
-datalongtoshort = pickle.load(f1)
-f1.close()
+f1 = open("long_to_short.txt", "r")
+f2 = open("10_data.txt",'r')
+f3 = open("data.txt",'a')
+dic = pickle.load(f1)
+lines = f2.readlines()
+for line in lines:
+	record = line.split('	')
+	longid=record[2]
+	print dic[longid]
+	string = str(record[0].split('_')[1].split(' ')[0])+"	"+str(dic[longid])
+	f3.write(string)
+f3.close()
