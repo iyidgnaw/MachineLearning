b6a72a9dbbbf0dbb885b2ad3460338b96b3eb32b
iyidgnaw <imwangdiyi@gmail.com>
2016-02-29 10:59:06 +0800

    2016 make it tidy.

--- NewRNN/how_to_load.py
+++
@@ -1,4 +0,0 @@
-import pickle
-f1 = open("xxxxxxxxxxxxx.txt", "r")
-datalongtoshort = pickle.load(f1)
-f1.close()
--- rnn/codefortest.py
+++
@@ -1,6 +0,0 @@
-import time
-
-
-ISOTIMEFORMAT='%Y-%m-%d %X'
-print "Start time is :"
-print time.strftime( ISOTIMEFORMAT, time.localtime( time.time() ) ) 
