daf142da611dea0c666158187932ac8a5ec84fba
iyidgnaw <imwangdiyi@gmail.com>
2016-03-22 15:58:15 +0800

    delete lizk

--- lizk/DayToWeek.py
+++
@@ -1,9 +0,0 @@
-import calendar
-
-year = 2014
-for line in open("mobile_short.csv"):
-	userid, itemid, month, day, hour = line.split(",")
-	month = int(month)
-	day = int(day)
-
-	weekday = calendar.weekday(year, month, day)
