b0f651a35e6a2918cd3f85262d7dd053b57034fb
iyidgnaw <imwangdiyi@gmail.com>
2016-03-23 10:34:15 +0800

    3.23 10:34

--- current/test.py
+++ current/test.py
@@ -1,16 +1,42 @@
+from multiprocessing import Process
+import os
+import time
 import numpy as np
 
-x = np.random.randn(1000, 10)
-hprev = np.ones((1, 10))
-predict_matrix = np.dot(hprev,x.T)
+def test1():
+	print 'process id:', os.getpid()
+	x = np.random.randn(1000, 10)
+	hprev = np.ones((1, 10))
+	predict_matrix = np.dot(hprev,x.T)
+	
+def test2():
+	x = np.random.randn(1000, 10)
+	hprev = np.ones((1, 10))
+	predict_matrix = np.dot(hprev,x.T)
 
-rank_index = np.argsort(predict_matrix, axis=1)
-rank_index = rank_index[:, -20:np.shape(rank_index)[1]]
-rank_index_list = list(reversed(list(rank_index[0])))
-print rank_index_list
+	
+	
 
+	
+t1 = time.clock()
+if __name__ == '__main__':
+	test1()
+	queue = []
+	for i in range(20):
+		p = Process(target=test1)
+		queue.append(p)
+	for i in range(20):
+		queue[i].start()
+	for i in range(20):
+		queue[i].join()
+	print "done"
+t2 =time.clock()
+print t2-t1
 
-rank = np.argpartition(predict_matrix[0],-20)[-20:]
-rank = rank[np.argsort(predict_matrix[0][rank])]
-rank_index_list = list(reversed(list(rank)))
-print rank_index_list
+t1 = time.clock()
+print 'process id:', os.getpid()
+for i in range(20):
+	test2()
+print "done"
+t2 =time.clock()
+print t2-t1
