e0322598b9940f896ec8234f827bf4732d1f3c59
iyidgnaw <imwangdiyi@gmail.com>
2016-03-28 12:12:09 +0800

    update

--- current/test.py
+++ current/test.py
@@ -1,8 +1,15 @@
-for line in open("../mobile_time.csv"):
+import json
+
+
+user_list=[]
+itemid_list = []
+for line in open("./mobile_time.csv"):
 	userid, artid, month, day, hour, time_sub = line.split(",")
 	userid = int(userid)
 	artid = int(artid)
 	time_sub = int(time_sub)
-	i += 1
 	user_list.append(int(userid))
 	itemid_list.append(int(artid))
+product_id = list(set(itemid_list))
+print product_id
+print len(product_id)
