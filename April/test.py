afe7691a7fb963c5dc0a83857e2918cbe72a6de3
iyidgnaw <imwangdiyi@gmail.com>
2016-04-06 09:31:50 +0800

    4.6 morning

--- April/test.py
+++ April/test.py
@@ -1,18 +1,7 @@
-import random
-import numpy as np
-
-ITEM_SIZE =1000
-HIDDEN_SIZE = 10
-def avg_neg(neglist):
-	Sum = np.zeros((1, HIDDEN_SIZE))
-	for i in neglist:
-		Sum +=X[i-1, :].reshape(1, HIDDEN_SIZE)
-		print Sum
-	return Sum/20
-
-
-X = np.random.randn(ITEM_SIZE, HIDDEN_SIZE)*0.5
-itemlist = range(ITEM_SIZE)
-neglist = random.sample(itemlist,20)
-item_neg = avg_neg(neglist)
-print item_neg
+import pickle
+basic = open('resultbasic.txt','rb')
+wdy = open('resultwdy.txt','rb')
+basic = pickle.load(basic)
+print basic
+wdy = pickle.load(wdy)
+print wdy
