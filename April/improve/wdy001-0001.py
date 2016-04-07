7a15f8b627a5326d50bd955427a088f96fe41cbb
iyidgnaw <imwangdiyi@gmail.com>
2016-04-07 10:15:39 +0800

    correct a huuuuuuuuuuuuge bug

--- April/improve/wdy001-0001.py
+++ April/improve/wdy001-0001.py
@@ -5,7 +5,7 @@ import random
 import json
 import pickle
 import sys
-
+import time
 
 USER_SIZE = 1904			# 总用户数
 ITEM_SIZE = 1157			# 总商品种数
@@ -110,7 +110,7 @@ def train(user_cart,time_cart):
 		# neglist = random.sample(itemlist,NEG_NUM)
 		# item_neg = avg_neg(neglist)
 		neg = random.randint(1, ITEM_SIZE)
-		while (i+1) == neg:
+		while user_cart[i+1] == neg:
 			neg = random.randint(1, ITEM_SIZE)
 
 		item_pos = X[user_cart[i+1]-1, :].reshape(1, HIDDEN_SIZE)		# positive sample's vector
@@ -122,10 +122,11 @@ def train(user_cart,time_cart):
 		# 计算状态t的h、dh
 		b = np.dot(item_curt, U) + np.dot(hl, Wp)
 		h = sigmoid(b)
+		
 
 
 
-		xi_j = np.dot(Wk,item_pos.T - item_neg.T)
+		xi_j = np.dot(Wk,(item_pos.T-item_neg.T))
 		xij = np.dot(h, xi_j)
 		loss += xij
 		# 若为tmp = sigmoid(-Xij) 则LEARNING_RATE和LAMBDA为负
@@ -154,6 +155,7 @@ def train(user_cart,time_cart):
 		item = X[user_cart[i] - 1, :].reshape(1, HIDDEN_SIZE)
 		hnminus2 = hiddenlist[i]
 		dh = dhlist[i] + dh1
+		Wp=WPLIST[time_cart[i]]
 
 		sumdu += np.dot(item.T, dh * midlist[i])
 		dWp = np.dot(hnminus2.T,dh*midlist[i])
@@ -227,11 +229,7 @@ def predict():
 
 
 
-# allrecord=[]
-# for i in xrange(len(all_cart)):
-# 	user_cart = all_cart[i]
-# 	for behavior in user_cart:
-# 		allrecord.append(behavior[0])
+
 def basic_info():
 	print "LEARNING_RATE = %f" % LEARNING_RATE
 	print "LAMBDA = %f" % LAMBDA
@@ -241,8 +239,8 @@ def learn():
 	global Pastrecall
 	ite = 0
 	while (ite<=400):
-		f_handler = open('4_6_result_001-0001.txt','a')
-		sys.stdout=f_handler	
+		# f_handler = open('4_6_result_001-0001.txt','a')
+		# sys.stdout=f_handler	
 		print "Iter %d" % ite
 		print "Training..."
 		sumloss = 0
@@ -253,14 +251,14 @@ def learn():
 			sumloss += loss
 		print "begin predict"
 		print sumloss
-		recall = predict()
+		# recall = predict()
 		# if recall[10]>Pastrecall[10]:
 		# 	result = open('bestparameter.txt','w')
 		# 	list1 = [recall,U,WKLIST,WPLIST,X]
 		# 	pickle.dump(list1,result)
 		# 	result.close()
 		# 	Pastrecall = recall
-		f_handler.close()
+		# f_handler.close()
 		ite += 1
 
 
