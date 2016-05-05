257228784e38a1cd268e1a6077daf146c85c09c0
iyidgnaw <imwangdiyi@gmail.com>
2016-05-05 09:42:52 +0800

    update

--- April/Context/Weekday.py
+++ April/Context/Weekday.py
@@ -6,28 +6,39 @@ import json
 import pickle
 import sys
 
-Pastrecall = {}
-Pastrecall[10]=0
+
 USER_SIZE = 1904			# 总用户数
 ITEM_SIZE = 1157			# 总商品种数
 HIDDEN_SIZE = 10			# hidden layer的维度
-LEARNING_RATE = 0.1 		# 学习速率
+LEARNING_RATE = 0.01 		# 学习速率
 LAMBDA = 0.001 				# 惩罚系数
 TOP = 20 					# recall取前Top个
-U = np.random.randn(HIDDEN_SIZE, HIDDEN_SIZE)*0.5
+
+H_ZERO = np.zeros((1, HIDDEN_SIZE))
 X = np.random.randn(ITEM_SIZE, HIDDEN_SIZE)*0.5
 W = np.random.randn(HIDDEN_SIZE, HIDDEN_SIZE)*0.5
-H_ZERO = np.zeros((1, HIDDEN_SIZE))
-
-DATAFILE = '../data/user_cart_basic.json'
+UWF = []
+UWS = []
+RECALL_MAX = {}		# 最优
+ITER_MAX = 0
+for i in range(TOP):
+	RECALL_MAX[i+1] = 0
+for i in range (7):
+	uw = np.random.randn(HIDDEN_SIZE, HIDDEN_SIZE)*0.5
+	UWF.append(uw)
+	UWS.append(uw)
+DATAFILE = '../data/user_cart_month.json'		# 路径文件名
 
 ITEM_TRAIN = {}
 ITEM_TEST = {}
+WEEKDAY_TRAIN = {}
+WEEKDAY_TEST = {}
 SPLIT = 0.9
 
 
-def sigmoid(x):
 
+
+def sigmoid(x):
 	output = 1.0/(1.0+np.exp(-x))
 	return output
 
@@ -50,47 +61,61 @@ def pre_data():
 		all_cart.append(line1)
 
 	for i in xrange(len(all_cart)):
-			item_train = []
-			item_test = []
-			behavior_list = all_cart[i]
-			behavior_train = behavior_list[0:int(SPLIT*len(behavior_list))]
-			behavior_test = behavior_list[int(SPLIT*len(behavior_list)):]
-			for behavior in behavior_train:
-				item_train.append(behavior[0])		# behavior中第一个位置为商品编号
-			for behavior in behavior_test:
-				item_test.append(behavior[0])
-			ITEM_TRAIN[i] = item_train
-			ITEM_TEST[i] = item_test
+		item_train = []
+		item_test = []
+		weekday_train = []
+		weekday_test = []
+		behavior_list = all_cart[i]
+		behavior_train = behavior_list[0:int(SPLIT*len(behavior_list))]
+		behavior_test = behavior_list[int(SPLIT*len(behavior_list)):]
+		for behavior in behavior_train:
+			item_train.append(behavior[0])		# behavior中第一个位置为商品编号
+			weekday_train.append(behavior[1])
+		for behavior in behavior_test:
+			item_test.append(behavior[0])
+			weekday_test.append(behavior[1])
+
+		ITEM_TRAIN[i] = item_train
+		ITEM_TEST[i] = item_test
+		WEEKDAY_TRAIN[i] = weekday_train
+		WEEKDAY_TEST[i] = weekday_test
 
 
-def train(user_cart):
+def train(user_cart, weekday_cart):
 	global U, W, X
 
 	dhlist = []							# bpr中对h的导数
 	hiddenlist = []						# 记录[1,T]状态hidden layer (不包括1的上一个状态的hidden layer)
-	midlist = []							# BPTT中传到第一层的导数 sigmoid(bi)*(1-sigmoid(bi))
+	midlist = []						# BPTT中传到第一层的导数 sigmoid(bi)*(1-sigmoid(bi))
 	hl = np.copy(H_ZERO)				# 初始化last hidden layer	
-	sumdu = 0 							# 记录对于每一个用户BPTT中u、w总	更新量
+	sumdUW = [] 						# 记录对于每一个用户BPTT中u、w总	更新量
 	sumdw = 0
 	loss = 0
+	for i in range(7):
+		sumdUW.append(0)
+
 	# BPR
-	dh1 = np.copy(H_ZERO)					# dh for the back process
+	dh1 = np.copy(H_ZERO)				# dh for the back process
 	
 	for i in xrange(len(user_cart)-1):
 		# 对于要预测的item进行负采样
 		neg = random.randint(1, ITEM_SIZE)
-		while (user_cart[i+1]) == neg:
+		while (i+1) == neg:
 			neg = random.randint(1, ITEM_SIZE)
 
 		item_pos = X[user_cart[i+1]-1, :].reshape(1, HIDDEN_SIZE)		# positive sample's vector
 		item_curt = X[user_cart[i]-1, :].reshape(1, HIDDEN_SIZE)		# current input vector
 		item_neg = X[neg-1, :].reshape(1, HIDDEN_SIZE)			# negative sample's vector
-		
+		weekday_now = weekday_cart[i]
+		weekday_next = weekday_cart[i+1]
+		uw_now = UWF[weekday_now]
+		uw_next = UWS[weekday_next]
+
 		# 计算状态t的h、dh
-		b = np.dot(item_curt, U) + np.dot(hl, W)
+		b = np.dot(item_curt, uw_now) + np.dot(hl, W)
 		h = sigmoid(b)
-		xi_j = item_pos.T - item_neg.T
-		xij = np.dot(h, xi_j)
+		xi_j = item_pos - item_neg
+		xij = np.dot(np.dot(h, uw_next), xi_j.T)
 		loss += xij
 		# 若为tmp = sigmoid(-Xij) 则LEARNING_RATE和LAMBDA为负
 		tmp = -(1 - sigmoid(xij))
@@ -98,32 +123,41 @@ def train(user_cart):
 		hiddenlist.append(h)
 		mid = h * (1 - h)
 		midlist.append(mid)	
-		dhlist.append(tmp * (item_pos - item_neg))		# save the dh for each bpr step
+		dhlist.append(tmp * np.dot(item_pos - item_neg, uw_next.T))		# save the dh for each bpr step
 
 		# 计算对于负样本的导数 并更新负样本的vector
-		dneg = -tmp * h + LAMBDA * item_neg
+		dneg = -tmp * np.dot(h, uw_next) + LAMBDA * item_neg
 		X[neg-1, :] += -LEARNING_RATE * (dneg.reshape(HIDDEN_SIZE, ))
 		# 计算对于正样本的导数 并更新正样本的vector
-		ditem = tmp * h + LAMBDA * item_pos
+		ditem = tmp * np.dot(h, uw_next) + LAMBDA * item_pos
 		X[user_cart[i+1]-1, :] += -LEARNING_RATE * (ditem.reshape(HIDDEN_SIZE,))
+		# 计算next UW的更新量
+		# dUW = tmp * np.dot((item_pos - item_neg).T, h)
+		dUWS = tmp * np.dot(h.T, (item_pos - item_neg))
+		UWS[weekday_next] += -LEARNING_RATE * (dUWS + LAMBDA * UWS[weekday_next])
 		# 更新last hidden layer
 		hl = h
 
 	# BPTT
 	for i in range(len(user_cart) - 1)[::-1]:
 		item = X[user_cart[i] - 1, :].reshape(1, HIDDEN_SIZE)
+		weekday_now = weekday_cart[i]
+		uw_now = UWF[weekday_now]
 		hnminus2 = hiddenlist[i]
 		dh = dhlist[i] + dh1
 
-		sumdu += np.dot(item.T, dh * midlist[i])
+		dUW = np.dot(item.T, dh * midlist[i])
+		sumdUW[weekday_now] += dUW
 		sumdw += np.dot(hnminus2.T, dh * midlist[i])
 		# 更新输入的样本
-		dx = np.dot(dh * midlist[i], U.T)
+		dx = np.dot(dh * midlist[i], uw_now.T)
 		X[user_cart[i]-1, :] += -LEARNING_RATE*(dx.reshape(HIDDEN_SIZE, ) + LAMBDA * X[user_cart[i]-1, :])
 
 		dh1 = np.dot(dh * midlist[i], W.T)
-	U += -LEARNING_RATE * (sumdu + LAMBDA * U)
+		
 	W += -LEARNING_RATE * (sumdw + LAMBDA * W)
+	for day in range(7):
+		UWF[day] += -LEARNING_RATE * (sumdUW[day] + LAMBDA * UWF[day])
 	return loss
 
 
@@ -138,30 +172,38 @@ def predict():
 		recall[i+1] = 0
 	
 	for n in ITEM_TEST.keys():
-		train = ITEM_TRAIN[n]
-		test = ITEM_TEST[n]
+		item_train = ITEM_TRAIN[n]
+		item_test = ITEM_TEST[n]
+		weekday_train = WEEKDAY_TRAIN[n]
+		weekday_test = WEEKDAY_TEST[n]
 		hl = np.copy(H_ZERO)
 		h = np.copy(H_ZERO)
 		# 计算需要预测的状态对应的hidden layer
-		for item_id in train:
-			item = X[item_id-1]
-			b = np.dot(item, U) + np.dot(hl, W)
+		for i in range(len(item_train)):
+			weekday_now = weekday_train[i]
+			uw = UWF[weekday_now]
+			item = X[item_train[i]-1]
+			b = np.dot(item, uw) + np.dot(hl, W)
 			h = sigmoid(b)
 			hl = h
 		# 预测
-		for j in xrange(len(test)):
+		for j in xrange(len(item_test)):
+			weekday_now = weekday_test[j]
+			#uwb = UWF[weekday_now]
+			uwb = UWS[weekday_now]
 			relevant += 1
-			predict_matrix = np.dot(h, X.T)
+			predict_matrix = np.dot(np.dot(h, uwb), X.T)
 			rank = np.argpartition(predict_matrix[0], -TOP)[-TOP:]
 			rank = rank[np.argsort(predict_matrix[0][rank])]
 			rank_index_list = list(reversed(list(rank)))
 
-			if test[j]-1 in rank_index_list:
-				index = rank_index_list.index(test[j]-1)
+			if item_test[j]-1 in rank_index_list:
+				index = rank_index_list.index(item_test[j]-1)
 				hit[index+1] += 1
 
-			item = X[test[j] - 1]
-			b = np.dot(item, U) + np.dot(h, W)
+			item = X[item_test[j] - 1]
+			uwf = UWF[weekday_now]
+			b = np.dot(item, uwf) + np.dot(h, W)
 			h = sigmoid(b)
 
 	for i in range(20):
@@ -173,11 +215,13 @@ def predict():
 	print relevant
 	print recall
 	print recallatx
-	return recall
+	return recall, recallatx
+
 
 def basic_info():
 	print "LEARNING_RATE = %f" % LEARNING_RATE
 	print "LAMBDA = %f" % LAMBDA
+
 
 def save_max(result, n, iter):
 	'''
@@ -191,23 +235,26 @@ def save_max(result, n, iter):
 
 	print "Best Result At Iter %i" %ITER_MAX
 	print RECALL_MAX
+	print
+
 
 def learn():
-	global Pastrecall
 	ite = 0
-	while (ite<=800):
-		f_handler = open('basic_10.txt','a')
+	while True:
+		f_handler = open('weekday0.txt','a')
 		sys.stdout=f_handler	
 		print "Iter %d" % ite
 		print "Training..."
 		sumloss = 0
 		for i in ITEM_TRAIN.keys():
 			user_cart = ITEM_TRAIN[i]
-			loss = train(user_cart)
+			weekday_cart = WEEKDAY_TRAIN[i]
+			loss = train(user_cart, weekday_cart)
 			sumloss += loss
+
 		print "begin predict"
 		print sumloss
-		
+
 		recall, recallatx = predict()
 		save_max(recallatx, 10, ite)
 		f_handler.close()
